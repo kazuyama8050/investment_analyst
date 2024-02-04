105d8fba68ecbfbc995f8b97abb70463ad17224f
kazuyama8050 <kazuki118050@gmail.com>
2024-02-04 16:51:54 +0900

    bugfix

--- fx_historical_data/download_fx_data.py
+++ fx_historical_data/download_fx_data.py
@@ -1,36 +1,41 @@
-try:
-    import os,io
-    import zipfile
-    import shutil
+import os,io
+import zipfile
+import shutil
 
-    from histdata import download_hist_data as dl
-    from histdata.api import Platform as P, TimeFrame as TF
+from histdata import download_hist_data as dl
+from histdata.api import Platform as P, TimeFrame as TF
 
-    import sys,traceback
-    args = sys.argv
+import sys,traceback
 
-    SYMBOL = args[1]
-    TERM = args[2]
-    START_YEAR = int(args[3])
-    END_YEAR = int(args[4])
+def main():
+    try:
+        args = sys.argv
 
-    term_mapping = {
-        "M1": TF.ONE_MINUTE
-    }
+        SYMBOL = args[1]
+        TERM = args[2]
+        START_YEAR = int(args[3])
+        END_YEAR = int(args[4])
 
-    if os.path.exists("./{}".format(SYMBOL)):
-        os.mkdir("./{}".format(SYMBOL))
+        term_mapping = {
+            "M1": TF.ONE_MINUTE
+        }
 
-    for year in range(START_YEAR,END_YEAR+1):
-        dl(year=year, month=None, pair=SYMBOL, platform=P.GENERIC_ASCII, time_frame=term_mapping[TERM])
+        if os.path.exists("./{}".format(SYMBOL)):
+            os.mkdir("./{}".format(SYMBOL))
 
-        file_prefix = "DAT_ASCII_{0}_{1}_{2}".format(SYMBOL.upper(), TERM, year)
-        shutil.unpack_archive(file_prefix+".zip")
-        shutil.move(file_prefix+".csv", "./{}/".format(SYMBOL))
-        os.remove(file_prefix+".txt")
-        os.remove(file_prefix+".zip")
+        for year in range(START_YEAR,END_YEAR+1):
+            dl(year=year, month=None, pair=SYMBOL, platform=P.GENERIC_ASCII, time_frame=term_mapping[TERM])
+
+            file_prefix = "DAT_ASCII_{0}_{1}_{2}".format(SYMBOL.upper(), TERM, year)
+            shutil.unpack_archive(file_prefix+".zip")
+            shutil.move(file_prefix+".csv", "./{}/".format(SYMBOL))
+            os.remove(file_prefix+".txt")
+            os.remove(file_prefix+".zip")
+            
+            print("{} done".format(year))
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
         
-        print("{} done".format(year))
-except Exception as e:
-    print(e)
-    print(traceback.format_exc())
+if __name__ == "__main__":
+    main()
